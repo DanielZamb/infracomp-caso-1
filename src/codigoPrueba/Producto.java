bd8e78ccd1c2f1f307ee5e87cf8b86d74159d716
Juanandres44 <ja.santiago>
2021-02-23 17:48:55 -0500

    Codigo Juan

---
+++ src/codigoPrueba/Producto.java
@@ -0,0 +1,16 @@
+public class producto {
+    
+    private int numero;
+
+    public Producto (int n){
+        n = this.numero;
+    }
+
+    public void aumentar(){
+
+    }
+
+    public int darNumero(){
+        return numero;
+    }
+}
